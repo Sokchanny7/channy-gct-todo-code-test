a71c24178d3c1c80b71a7c76d01a21b8c10acb18
Sokchanny7 <sokchanny.sg@gmail.com>
2021-07-25 17:19:58 +0700

    2 files modified

--- src/components/todo_controller.js
+++ src/components/todo_controller.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import _ from 'lodash';
 const TodoController = props => {
   return (
     <div className="todo-controller">
       <div className="w-80">
-        <input ref={props.inputRef} type="text" onChange={e => {}} />
+        <input
+          ref={props.inputRef}
+          type="text"
+          onChange={e => {
+            props.onChange && props.onChange(e.target.value);
+          }}
+          value={_.get(props, 'value')}
+        />
       </div>
       <div className="w-20">
-        <button onClick={props.handleAdd}>Add</button>
+        <button
+          onClick={() => {
+            props.handleAdd && props.handleAdd();
+          }}
+        >
+          Add
+        </button>
       </div>
     </div>
   );
--- src/pages/todo_page.js
+++ src/pages/todo_page.js
@@ -3,6 +3,7 @@ import AppConfig from '../config/app_config';
 import _ from 'lodash';
 import TodoList from '../components/todo_list';
 import TodoController from '../components/todo_controller';
+import moment from 'moment';
 const TodoPage = props => {
   const inputRef = useRef(null);
   const [todoList, setTodoList] = useState([]);
@@ -12,20 +13,40 @@ const TodoPage = props => {
     fetchTodoList();
   }, []);
 
+  useEffect(() => {
+    if (todoList != undefined) {
+      localStorage.setItem(
+        AppConfig.MY_TODO_LIST_KEY,
+        JSON.stringify(todoList)
+      );
+    }
+  }, [todoList]);
+
   const fetchTodoList = () => {
-    const list = localStorage.getItem(AppConfig.MY_TODO_LIST_KEY);
-    console.log('>list', list);
-    // if (list != undefined && list.length > 0) setTodoList(list);
+    const object = localStorage.getItem(AppConfig.MY_TODO_LIST_KEY);
+    if (object != undefined) setTodoList(JSON.parse(object));
   };
 
   const handleAdd = () => {
-    localStorage.setItem(AppConfig.MY_TODO_LIST_KEY, [...todoList, todoText]);
+    setTodoList([
+      ...(todoList ?? []),
+      {
+        id: todoList != undefined ? todoList.length + 1 : 1,
+        date: moment().format('MM-DD-YYYY hh:mm'),
+        value: todoText
+      }
+    ]);
   };
 
   return (
     <div>
       <TodoList todoList={todoList} />
-      <TodoController inputRef={inputRef} />
+      <TodoController
+        handleAdd={handleAdd}
+        inputRef={inputRef}
+        onChange={setTodoText}
+        value={todoText}
+      />
     </div>
   );
 };
